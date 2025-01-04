41c92dbca93ac46542c45ca38cc718fe51041fb3
Anand  Kumar Das <81643298+Anand370@users.noreply.github.com>
2025-01-04 18:42:27 +0530

    Update auth.config.js

--- src/app/api/config/auth.config.js
+++ src/app/api/config/auth.config.js
@@ -1,4 +1,21 @@
+import jwt from "jsonwebtoken";
+
 module.exports = {
-    secret: "bezkoder-secret-key"
+    secret: "bezkoder-secret-key",
+    verifyToken : (token,secret) => {
+      try {
+        return jwt.verify(token, secret);
+      } catch (error) {
+        throw new Error('Invalid or expired token');
+      }
+    },
+    generateToken : (email,role,secret) => {
+      const payload = {
+        role: role,
+        email: user.email,
+      };
+    
+      return jwt.sign(payload, secret, { expiresIn: 86400 }); // 24h
+    }
   };
   
