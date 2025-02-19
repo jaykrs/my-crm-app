9fcb33112c945cb2b0e82bff67f398ee27f3a6dd
= <=>
2025-02-19 22:36:34 +0530

    grapes edistor as template editor added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,12 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <link href="https://unpkg.com/grapesjs/dist/css/grapes.min.css" rel="stylesheet" />
+        <script src="https://unpkg.com/grapesjs"></script>
+        <script src="https://unpkg.com/grapesjs-tailwind"></script>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
       <body suppressHydrationWarning={true}>
         <div className="dark:bg-boxdark-2 dark:text-bodydark">
           {loading ? <Loader /> : children}
